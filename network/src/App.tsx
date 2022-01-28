4798568c72fda9f48f62c31222447c6498f37465
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-01-28 11:34:17 +0300

    28.01

--- network/src/App.tsx
+++ network/src/App.tsx
@@ -4,11 +4,12 @@ import s from './App.module.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
+import Dialogs  from "./components/Dialogs/Dialogs";
 import {Route, Routes} from 'react-router-dom';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+
 
 const App = () => {
     return (
@@ -20,10 +21,10 @@ const App = () => {
                 <div className={s.app_wrapper_content}>
                     <Routes>
                     <Route path={"/profile"} element={<Profile/>}/>
-                    <Route path={"/messages"} element={<Dialogs/>}/>
+                    <Route path={"/messages"} element={<Dialogs />}/>
                         <Route path={"/news"} element={<News/>}/>
                         <Route path={"/music"} element={<Music/>}/>
-                        <Route path={"/seatings"} element={<Settings/>}/>
+                        <Route path={"/settings"} element={<Settings/>}/>
                     </Routes>
                 </div>
 
--- network/src/components/Profile/MyPosts/Post.tsx
+++ network/src/components/Profile/MyPosts/Post.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import s from "./Post.module.css"
-const Post = () => {
+
+type PostType = {
+    id:number
+    message:string
+    likeCount:number
+
+}
+
+const Post = (props:PostType) => {
     return (
         <div className={s.item}>
         <img src={"https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"}/>
-    Post1
-            <span>Like</span>
+            {props.message}
+            <span>Like</span>{props.likeCount}
     </div>
     );
 };
